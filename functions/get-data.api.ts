148c2faa0e422bbe0b279076a2c167adc70bdd2c
Vladi <v-ed@outlook.com>
2023-01-10 00:12:19 +0200

    need tests

--- functions/get-data.api.ts
+++ functions/get-data.api.ts
@@ -1,8 +1,7 @@
-export const onGetRequest: PagesFunction<string> = async ({ request, env }) => {
+export const onGetRequest: PagesFunction<string> = async ({ request }) => {
   const urlPrams = new URLSearchParams(request.url);
   if (!urlPrams.has('service')) return new Response('Please provide service name');
 
-  const today = new Date().toISOString().split('T').shift();
 
   if (urlPrams.get('service') === 'aliya') {
     const serviceId = urlPrams.get('serviceId') || 9981; // Aliya benefits (Юля Ардити)
@@ -11,38 +10,21 @@ export const onGetRequest: PagesFunction<string> = async ({ request, env }) => {
     // const serviceId = 3123; // betuah leumi allowances  (ramat gan)
     // const serviceId = 3105; // betuah leumi allowances (tel-aviv)
 
-    const respDates = await fetch("https://central.myvisit.com/CentralAPI/SearchAvailableDates?serviceId=" + serviceId + "&startDate=" + today, {
-      "headers": {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-        "application-api-key": "8640a12d-52a7-4c2a-afe1-4411e00e3ac4",
-        "application-name": "myVisit.com v3.5",
-        "Origin": "https://myvisit.com",
-        "Cookie": "rbzsessionid=6c2514bec619b4d0b4f9b1fedce2325e; mvcnm=Israel; mvcid=1; mvcc=il; GCLB=COvQjvqO-LyQ0gE; rbzid=PRV3vlnCcaAf+hMhKWrqUfkLKOTNxUXmqoFNjxsH9DtR3J2JgDgaYKIJdMqUhnBClnRYrZDGV7Xm0wuNbSHbIhbV63d9pYmyWfTGYOD6ILc0NoraXMvgpve13yLIi5ozpGI8Ftp0psFK3VfDbKtiXnBAEL7BFtslAfZkNOzQP915rg+Vcj9q/YiYUBVRroDQkWgLx+ToqxmGlQ0eL8VF/2Oy6bghN28MQ0WmyfO0Hh4=; _gid=GA1.2.522615553.1673114473; ARRAffinity=b1ef89074c71e161dc4fc9e1fb894062be585b15fd3db58dcfd3dfb656c73999; ARRAffinitySameSite=b1ef89074c71e161dc4fc9e1fb894062be585b15fd3db58dcfd3dfb656c73999; CentralJWTCookie=jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im92VFc2ckQ4ZmExM1V1cUdKT1BQNkFqa2NMQSJ9.eyJpc3MiOiJodHRwOi8vY2VudHJhbC5xbm9teS5jb20iLCJhdWQiOiJodHRwOi8vY2VudHJhbC5xbm9teS5jb20iLCJuYmYiOjE2NzMyNTE0MjQsImV4cCI6MTY3MzI1NTAyNCwidW5pcXVlX25hbWUiOiI4YTViMjNkMi0xYTk3LTQzMDUtYTJjZC1kYTU3MTAzNzJhNDciLCJ1aWQiOiJHb2dIRXlGR0kyZitVdGFOWGZIVDFRPT0ifQ.O8RLFHZqMWtkh_8dtRpoy1Q6kAesUTKUWacUSBVwZkLnYPMtHfGBv8D-TZF_3P-886P737rPyDFPHW6-Zm1Vhtwe3TtOsv0IAvxpZV-8sgX8X1ot2wxUQoj2fobbx5yklAotF0ojtjbDN-5SipSx714_Aq6Nen3z9IpWwFDBav55kyEDqOVjumApLPhwBDw_sOlvk4Jr9CVvfW6G1F3ZEN4xzc99U8TWfpzswQAG-20VBeJiArPBuQdjNzjv49UiynKnbt70crObeaB1r8vg38W1ePZAsuatb0CeTWfvGoavnYd2yrKGGDUDuENekPhpn-SMho2SvmvIqoCVnPPr3A",
-      }
-    }) as any;
+    const cookie = request.headers.get('Cookie') || '';
 
-    const respDatesResults = (await respDates.json()).Results;
-    if (!respDatesResults) return new Response('Token expired');
+    const respDatesResults = await searchAvailableDates(serviceId, cookie);
+    if (!respDatesResults) return new Response('Token expired or empty');
 
     console.log('first results:', respDatesResults);
-    const firstRes = respDatesResults[0].calendarId;
+    const firstCalendarId = respDatesResults[0].calendarId;
 
 
-    const respTime = await fetch("https://central.myvisit.com/CentralAPI/SearchAvailableSlots?CalendarId=" + firstRes + "&ServiceId=" + serviceId + "&dayPart=0", {
-      "headers": {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-        "application-api-key": "8640a12d-52a7-4c2a-afe1-4411e00e3ac4",
-        "application-name": "myVisit.com v3.5",
-        "Origin": "https://myvisit.com",
-        "Cookie": "rbzsessionid=6c2514bec619b4d0b4f9b1fedce2325e; mvcnm=Israel; mvcid=1; mvcc=il; GCLB=COvQjvqO-LyQ0gE; rbzid=PRV3vlnCcaAf+hMhKWrqUfkLKOTNxUXmqoFNjxsH9DtR3J2JgDgaYKIJdMqUhnBClnRYrZDGV7Xm0wuNbSHbIhbV63d9pYmyWfTGYOD6ILc0NoraXMvgpve13yLIi5ozpGI8Ftp0psFK3VfDbKtiXnBAEL7BFtslAfZkNOzQP915rg+Vcj9q/YiYUBVRroDQkWgLx+ToqxmGlQ0eL8VF/2Oy6bghN28MQ0WmyfO0Hh4=; _gid=GA1.2.522615553.1673114473; ARRAffinity=b1ef89074c71e161dc4fc9e1fb894062be585b15fd3db58dcfd3dfb656c73999; ARRAffinitySameSite=b1ef89074c71e161dc4fc9e1fb894062be585b15fd3db58dcfd3dfb656c73999; CentralJWTCookie=jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im92VFc2ckQ4ZmExM1V1cUdKT1BQNkFqa2NMQSJ9.eyJpc3MiOiJodHRwOi8vY2VudHJhbC5xbm9teS5jb20iLCJhdWQiOiJodHRwOi8vY2VudHJhbC5xbm9teS5jb20iLCJuYmYiOjE2NzMyNTE0MjQsImV4cCI6MTY3MzI1NTAyNCwidW5pcXVlX25hbWUiOiI4YTViMjNkMi0xYTk3LTQzMDUtYTJjZC1kYTU3MTAzNzJhNDciLCJ1aWQiOiJHb2dIRXlGR0kyZitVdGFOWGZIVDFRPT0ifQ.O8RLFHZqMWtkh_8dtRpoy1Q6kAesUTKUWacUSBVwZkLnYPMtHfGBv8D-TZF_3P-886P737rPyDFPHW6-Zm1Vhtwe3TtOsv0IAvxpZV-8sgX8X1ot2wxUQoj2fobbx5yklAotF0ojtjbDN-5SipSx714_Aq6Nen3z9IpWwFDBav55kyEDqOVjumApLPhwBDw_sOlvk4Jr9CVvfW6G1F3ZEN4xzc99U8TWfpzswQAG-20VBeJiArPBuQdjNzjv49UiynKnbt70crObeaB1r8vg38W1ePZAsuatb0CeTWfvGoavnYd2yrKGGDUDuENekPhpn-SMho2SvmvIqoCVnPPr3A",
-      }
-    });
-
-    const timeArray = (await respTime.json()) as {Results: any[]}
+    let timeArrayResults = await searchAvailableSlots(firstCalendarId, serviceId , cookie);
     // console.log(timeArray);
+    if (timeArrayResults?.length === 0) timeArrayResults = await searchAvailableSlots(respDatesResults[1].calendarId, serviceId , cookie);
 
 
-    const newTimeArr = timeArray.Results?.map(obj => String(obj.Time / 60).replace('.5', ':30'))
+    const newTimeArr = timeArrayResults?.map(obj => String(obj.Time / 60).replace('.5', ':30'))
     console.log(newTimeArr);
 
     return new Response(JSON.stringify({date: respDatesResults[0].calendarDate.split('T').shift(), newTimeArr}))
@@ -85,15 +67,43 @@ export const onGetRequest: PagesFunction<string> = async ({ request, env }) => {
 //         "Accept-Language": "en",
 //         "Application-Name": "myVisit.com v3.5",
 //         "Application-API-Key": "8640a12d-52a7-4c2a-afe1-4411e00e3ac4",
-//         "PreparedVisitToken": "3810e58e-dc4e-44e9-b4e7-a2f61be7ee4f",
-//         "Sec-Fetch-Dest": "empty",
-//         "Sec-Fetch-Mode": "cors",
-//         "Sec-Fetch-Site": "same-site",
-//         "Pragma": "no-cache",
-//         "Cache-Control": "no-cache"
+//         "PreparedVisitToken": "3810e58e-dc4e-44e9-b4e7-a2f61be7ee4f"
 //     }
 // });
 
 
 
+}
+
+
+async function searchAvailableDates(serviceId: string | number, cookie: string) {
+  const today = new Date().toISOString().split('T').shift();
+
+  const data: { Results: { calendarId: number, calendarDate: string }[] } =
+    await fetch("https://central.myvisit.com/CentralAPI/SearchAvailableDates?serviceId=" + serviceId + "&startDate=" + today, {
+      headers: {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+        "application-api-key": "8640a12d-52a7-4c2a-afe1-4411e00e3ac4",
+        "application-name": "myVisit.com v3.5",
+        "Origin": "https://myvisit.com",
+        cookie
+      }
+    }).then(resp => resp.json());
+
+  return data.Results;
+}
+
+async function searchAvailableSlots(calendarId: number, serviceId: string | number, cookie: string) {
+
+  const respTime = await fetch("https://central.myvisit.com/CentralAPI/SearchAvailableSlots?CalendarId=" + calendarId + "&ServiceId=" + serviceId + "&dayPart=0", {
+    "headers": {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+      "application-api-key": "8640a12d-52a7-4c2a-afe1-4411e00e3ac4",
+      "application-name": "myVisit.com v3.5",
+      "Origin": "https://myvisit.com",
+      cookie
+    }
+  }).then(resp => resp.json()) as {Results: { Time: number }[]};
+
+  return respTime.Results;
 }
